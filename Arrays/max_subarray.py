13b29a3e3bc58f05828bba34809184451372d043
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-23 17:03:03 +0530

    Update max_subarray.py

--- Arrays/max_subarray.py
+++ Arrays/max_subarray.py
@@ -1,1 +1,13 @@
+def max_subarray_sum(nums):
+    max_sum = float('-inf')
+    current_sum = 0
+
+    for n in nums:
+        current_sum += n
+        max_sum = max(max_sum, current_sum)
+
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
 
