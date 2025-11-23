2a8fb3c0afc8d75a0d7a4a03a69f6eea6268e97f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-23 18:40:05 +0530

    Create longest_substring.py

---
+++ Strings/longest_substring.py
@@ -0,0 +1,11 @@
+def lengthOfLongestSubstring(s):
+    seen = {}
+    left = 0
+    longest = 0
+
+    for right, char in enumerate(s):
+        if char in seen and seen[char] >= left:
+            left = seen[char] + 1
+        seen[char] = right
+        longest = max(longest, right - left + 1)
+    return longest
