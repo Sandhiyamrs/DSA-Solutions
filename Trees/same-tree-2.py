a363b202666582974115551ae6d119356983cb0f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-25 16:20:19 +0530

    Create same-tree-2.py

---
+++ Trees/same-tree-2.py
@@ -0,0 +1,6 @@
+def is_same_tree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+    return is_same_tree(p.left, p.right) and is_same_tree(q.left, q.right)
