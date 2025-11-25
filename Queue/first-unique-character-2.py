7344ccbe2cdcbe3c88a47a03f7cf04f30f84ce1b
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-25 16:22:43 +0530

    Create first-unique-character-2.py

---
+++ Queue/first-unique-character-2.py
@@ -0,0 +1,11 @@
+def first_unique_char(s):
+    count = {}
+
+    for ch in s:
+        count[ch] = count.get(ch, 0) + 1
+
+    for i, ch in enumerate(s):
+        if count[ch] == 1:
+            return i
+
+    return -1
