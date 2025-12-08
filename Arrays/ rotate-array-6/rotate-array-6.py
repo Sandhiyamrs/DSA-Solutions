5ac93b3193cccaff86e81b4b2802d97b2f16f965
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-08 22:46:51 +0530

    Create rotate-array-6.py

---
+++ Arrays/ rotate-array-6/rotate-array-6.py
@@ -0,0 +1,10 @@
+def rotate(nums, k):
+    n = len(nums)
+    k %= n
+    nums[:] = nums[-k:] + nums[:-k]
+    return nums
+
+# Example usage
+nums = [1,2,3,4,5,6,7]
+k = 3
+print("Output:", rotate(nums, k))
