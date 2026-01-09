c26afff569a99d45fe5110dd8a7e377179895756
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:04:12 +0530

    Update combination-sum.py

--- Backtracking/combination-sum.py
+++ Backtracking/combination-sum.py
@@ -1,15 +1,14 @@
 def combinationSum(candidates, target):
     res = []
 
-    def backtrack(start, path, total):
+    def dfs(start, path, total):
         if total == target:
             res.append(path[:])
             return
         if total > target:
             return
-
         for i in range(start, len(candidates)):
-            backtrack(i, path + [candidates[i]], total + candidates[i])
+            dfs(i, path + [candidates[i]], total + candidates[i])
 
-    backtrack(0, [], 0)
+    dfs(0, [], 0)
     return res
