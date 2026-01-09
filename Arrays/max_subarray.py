731cd9b9762d0b931fe9d074ffe6fbb5806359f1
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:01:33 +0530

    Update max_subarray.py

--- Arrays/max_subarray.py
+++ Arrays/max_subarray.py
@@ -1,18 +1,6 @@
-def max_subarray_sum(nums):
-    max_sum = float('-inf')
-    current_sum = 0
-
-    for n in nums:
-        current_sum += n
-        max_sum = max(max_sum, current_sum)
-
-        if current_sum < 0:
-            current_sum = 0
-
-    return max_sum
-
-
-# Example run
-if __name__ == "__main__":
-    arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-    print("Maximum Subarray Sum:", max_subarray_sum(arr))
+def maxSubArray(nums):
+    curr = best = nums[0]
+    for n in nums[1:]:
+        curr = max(n, curr + n)
+        best = max(best, curr)
+    return best
