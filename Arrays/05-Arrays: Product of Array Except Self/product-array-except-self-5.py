547dc3cc4b2d49b0f892858b93221a3d12d66e75
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:00:32 +0530

    Update product-array-except-self-5.py

--- Arrays/05-Arrays: Product of Array Except Self/product-array-except-self-5.py
+++ Arrays/05-Arrays: Product of Array Except Self/product-array-except-self-5.py
@@ -1,6 +1,6 @@
-def product_except_self(nums):
+def productExceptSelf(nums):
     n = len(nums)
-    res = [1]*n
+    res = [1] * n
 
     left = 1
     for i in range(n):
@@ -8,11 +8,8 @@ def product_except_self(nums):
         left *= nums[i]
 
     right = 1
-    for i in range(n-1, -1, -1):
+    for i in range(n - 1, -1, -1):
         res[i] *= right
         right *= nums[i]
 
     return res
-
-# Test
-print(product_except_self([1,2,3,4]))  # Output: [24,12,8,6]
