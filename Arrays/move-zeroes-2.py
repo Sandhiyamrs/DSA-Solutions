5a03cf05de2378ce0c35affd4f132d7e04026e23
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:01:53 +0530

    Update move-zeroes-2.py

--- Arrays/move-zeroes-2.py
+++ Arrays/move-zeroes-2.py
@@ -1,13 +1,8 @@
-def move_zeroes(nums):
-    insert_pos = 0
-
-    for n in nums:
-        if n != 0:
-            nums[insert_pos] = n
-            insert_pos += 1
-
-    while insert_pos < len(nums):
-        nums[insert_pos] = 0
-        insert_pos += 1
-
-    return nums
+def moveZeroes(nums):
+    idx = 0
+    for num in nums:
+        if num != 0:
+            nums[idx] = num
+            idx += 1
+    for i in range(idx, len(nums)):
+        nums[i] = 0
