049af61247f697a8eb78726879126408885167fd
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 20:58:11 +0530

    Update knapsack.py

--- AdvancedDP/knapsack.py
+++ AdvancedDP/knapsack.py
@@ -1,13 +1,13 @@
-def knapsack(weights, values, W):
-    n = len(weights)
-    dp = [[0]*(W+1) for _ in range(n+1)]
-    for i in range(1, n+1):
-        for w in range(W+1):
-            if weights[i-1] <= w:
-                dp[i][w] = max(dp[i-1][w], values[i-1] + dp[i-1][w-weights[i-1]])
-            else:
-                dp[i][w] = dp[i-1][w]
-    return dp[n][W]
+def knapsack(W, wt, val, n):
+    dp = [[0] * (W + 1) for _ in range(n + 1)]
 
-# Example usage
-print(knapsack([1,3,4,5],[1,4,5,7],7))  # 9
+    for i in range(1, n + 1):
+        for w in range(W + 1):
+            if wt[i - 1] <= w:
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                )
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][W]
