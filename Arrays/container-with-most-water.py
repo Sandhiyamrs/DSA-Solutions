987f8dae5a04e02650f7447c2aec0a1f4674b504
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:00:53 +0530

    Update container-with-most-water.py

--- Arrays/container-with-most-water.py
+++ Arrays/container-with-most-water.py
@@ -1,12 +1,11 @@
 def maxArea(height):
     l, r = 0, len(height) - 1
-    max_area = 0
+    ans = 0
 
     while l < r:
-        max_area = max(max_area, min(height[l], height[r]) * (r - l))
+        ans = max(ans, min(height[l], height[r]) * (r - l))
         if height[l] < height[r]:
             l += 1
         else:
             r -= 1
-
-    return max_area
+    return ans
