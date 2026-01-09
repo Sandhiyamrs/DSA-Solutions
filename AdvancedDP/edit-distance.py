1ab1e5a41e893e3dbfa54ebd8be240b6d6653177
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 20:57:44 +0530

    Update edit-distance.py

--- AdvancedDP/edit-distance.py
+++ AdvancedDP/edit-distance.py
@@ -13,8 +13,8 @@ def minDistance(word1: str, word2: str) -> int:
                 dp[i][j] = dp[i - 1][j - 1]
             else:
                 dp[i][j] = 1 + min(
-                    dp[i - 1][j],
-                    dp[i][j - 1],
-                    dp[i - 1][j - 1]
+                    dp[i - 1][j],     # delete
+                    dp[i][j - 1],     # insert
+                    dp[i - 1][j - 1]  # replace
                 )
     return dp[m][n]
