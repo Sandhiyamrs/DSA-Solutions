1852fecb9c3b4d4c283f9c8f8614cb52be518221
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:07:14 +0530

    Update design-twitter.py

--- DesignProblems/design-twitter.py
+++ DesignProblems/design-twitter.py
@@ -1,22 +1,23 @@
-from collections import defaultdict, deque
+import heapq
+from collections import defaultdict
 
 class Twitter:
     def __init__(self):
         self.time = 0
-        self.tweets = defaultdict(deque)
+        self.tweets = defaultdict(list)
         self.following = defaultdict(set)
 
     def postTweet(self, userId, tweetId):
-        self.tweets[userId].appendleft((self.time, tweetId))
+        self.tweets[userId].append((self.time, tweetId))
         self.time += 1
 
     def getNewsFeed(self, userId):
-        feed = []
+        heap = []
         users = self.following[userId] | {userId}
         for u in users:
-            feed.extend(self.tweets[u])
-        feed.sort(reverse=True)
-        return [t[1] for t in feed[:10]]
+            for t in self.tweets[u][-10:]:
+                heapq.heappush(heap, t)
+        return [t[1] for t in heapq.nlargest(10, heap)]
 
     def follow(self, followerId, followeeId):
         self.following[followerId].add(followeeId)
