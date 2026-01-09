423ac1cdf2354495f6fd50ae3518b28181fedeb9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 20:59:41 +0530

    Update floyd-warshall.py

--- AdvancedGraph/floyd-warshall.py
+++ AdvancedGraph/floyd-warshall.py
@@ -1,10 +1,9 @@
 def floyd_warshall(graph):
-    n = len(graph)
-    dist = [[graph[i][j] for j in range(n)] for i in range(n)]
+    V = len(graph)
+    dist = [row[:] for row in graph]
 
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-
     return dist
