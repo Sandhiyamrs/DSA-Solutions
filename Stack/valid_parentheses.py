14b445679f3bc531918050193fe897034c0468cd
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 21:09:28 +0530

    Update valid_parentheses.py

--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -1,11 +1,12 @@
-def is_valid(s):
+def isValid(s: str) -> bool:
     stack = []
-    pair = {')':'(', '}':'{', ']':'['}
+    mapping = {')': '(', '}': '{', ']': '['}
 
     for ch in s:
-        if ch in pair:
-            if not stack or stack.pop() != pair[ch]:
+        if ch in mapping:
+            if not stack or stack.pop() != mapping[ch]:
                 return False
         else:
             stack.append(ch)
+
     return not stack
