a6482b393e04e2ad2a4ccf78aca8a3a76e704a7d
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 20:58:30 +0530

    Update matrix-chain-multiplication.py

--- AdvancedDP/matrix-chain-multiplication.py
+++ AdvancedDP/matrix-chain-multiplication.py
@@ -1,11 +1,13 @@
+import sys
+
 def matrixChainOrder(p):
     n = len(p)
     dp = [[0] * n for _ in range(n)]
 
-    for L in range(2, n):
-        for i in range(1, n - L + 1):
-            j = i + L - 1
-            dp[i][j] = float('inf')
+    for l in range(2, n):
+        for i in range(1, n - l + 1):
+            j = i + l - 1
+            dp[i][j] = sys.maxsize
             for k in range(i, j):
                 cost = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j]
                 dp[i][j] = min(dp[i][j], cost)
